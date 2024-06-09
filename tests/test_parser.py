4e797a1591d0697011045de1b9ca4d065200411d
tve1n <timarumak@gmail.com>
2024-06-09 12:50:26 +0500

    write test parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 import os
 from gendiff.parser import parse_data_from_file
@@ -9,14 +10,14 @@ FIXTURE_DIR = os.path.join('tests', 'fixtures')
 def get_file_path(file):
     return os.path.join(FIXTURE_DIR, file)
 
-@pytest.mark.parametrize('file, file content', [
-    ('file1.json', 'file1.txt'),
-    ('file1.yaml', 'file1.txt'),
-    ('file2.json', 'file2.txt'),
-    ('file2.yaml', 'file2.txt')
+@pytest.mark.parametrize('file, expect_result', [
+    ('file1.json', 'expect_result_file1.json'),
+    ('file1.yaml', 'expect_result_file1.json'),
+    ('file2.json', 'expect_result_file2.json'),
+    ('file2.yaml', 'expect_result_file2.json')
 ])
-def test_parse_data_from_file(file, file_content):
+def test_parse_data_from_file(file, expect_result):
     file_path = get_file_path(file)
-    file_content_path = get_file_path(file_content)
-    with open(file_content_path, 'r') as out_res:
-        assert parse_data_from_file(file_path) == out_res.read()
+    result_path = get_file_path(expect_result)
+    with open(result_path, 'r') as result:
+        assert parse_data_from_file(file_path) == json.loads(result.read())
