161caf8f0bf2dbe737ccbd6466578e3b6628d44e
Tve1n <timarumak@gmail.com>
2024-07-20 16:23:15 +0500

    Fix code

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,11 @@
 from gendiff.parser import parse_data_from_file
 from gendiff.generator import generate
-from gendiff.formatters.stylish import format_diff_stylish
+from gendiff.formatters.choice_formatter import format_diff
 
 def generate_diff(file1, file2, formatter='stylish'):
     obj_f1 = parse_data_from_file(file1)
     obj_f2 = parse_data_from_file(file2)
     diff = generate(obj_f1, obj_f2)
-    return format_diff_stylish(diff)
+    return format_diff(diff, formatter)
 
 #print(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json'))
