6e6e1b7554e6769ba457e50e4e16d85e4a54ba2f
Tve1n <timarumak@gmail.com>
2024-07-23 22:04:54 +0500

    !

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,5 +8,3 @@ def generate_diff(file1, file2, formatter='stylish'):
     obj_f2 = parse_data_from_file(file2)
     diff = generate(obj_f1, obj_f2)
     return format_diff(diff, formatter)
-
-# print(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain'))  # noqa: E501
